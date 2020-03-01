1846a82603cc3cfc56624c6b2b452edd97a898fa
FlorianBreut <florian.breut@gmail.com>
2020-03-01 16:03:29 +0100

    Update LRMate200i.m

--- LRMate200i/LRMate200i.m
+++ LRMate200i/LRMate200i.m
@@ -1,16 +1,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%----------------------------TP 2 LR Mate 200i-----------------------------
-
-% Breut Florian
-% MIQ4
+%----------------------------LR Mate 200i-----------------------------
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%définition des organes du robot à partir du paramétrage du
+%dÃ©finition des organes du robot Ã  partir du paramÃ©trage du
 %Denavit-Hartenberg
 L1=link([pi/2 150 0 350 0], 'standard');
 L2=link([0 250 pi/2 0 0], 'standard');
@@ -19,43 +16,43 @@ L4=link([pi/2 0 pi  290 0], 'standard');
 L5=link([pi/2 0 pi 0 0], 'standard');
 L6=link([0 0  0 100 0], 'standard');
 
-%déclaration du robot
+%dÃ©claration du robot
 LRMate200i=robot({L1 L2 L3 L4 L5 L6});
 LRMate200i.name = 'FANUC LR Mate 200i';
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%-------------------Calcul du modèle géométrique direct--------------------
+%-------------------Calcul du modÃ¨le gÃ©omÃ©trique direct--------------------
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%fkine : calcule la matrice homogène correspondant au passage à la position 
-%et à l'orientation de l'organe en bout de chaine.
+%fkine : calcule la matrice homogÃ¨ne correspondant au passage Ã  la position 
+%et Ã  l'orientation de l'organe en bout de chaine.
 
 %tr2rpy : donne l'angle de roulis,lacet, tangage extraits de la matrice
-%homogène calculée.
+%homogÃ¨ne calculÃ©e.
 
-%Tn(1:3,4)' correspond à la translation exprimée sous forme d'un vecteur
+%Tn(1:3,4)' correspond Ã  la translation exprimÃ©e sous forme d'un vecteur
 
 
-%Définition des postitions articulaires
+%DÃ©finition des postitions articulaires
 q1=[0 90 0 180 180 0]*pi/180;
 q2=[45 60 30 120 170 45]*pi/180;
 q3=[90 30 90 90 90 90]*pi/180;
 
-%Calcul du modèle géométrique direct pour la première configuration
-plot(LRMate200i,q1)%Trace la configuration du robot calculée pour q1
+%Calcul du modÃ¨le gÃ©omÃ©trique direct pour la premiÃ¨re configuration
+plot(LRMate200i,q1)%Trace la configuration du robot calculÃ©e pour q1
 axis('equal')
 title('Configuration q1 = [0 90 0 180 180 0]');
 T1=fkine(LRMate200i,q1);%Calcul la matrice de rotation
 disp('Configuration q1 = [0 90 0 180 180 0]');
-X1=[T1(1:3,4)' tr2rpy(T1)*180/pi]%Calcul des coordonnées opérationnels
+X1=[T1(1:3,4)' tr2rpy(T1)*180/pi]%Calcul des coordonnÃ©es opÃ©rationnels
 disp('appuyer sur une ENTREE')
 pause();%mise en pause du programme pour 2 secondes
 clf
 
-%Calcul du modèle géométrique direct pour la configuration 2
+%Calcul du modÃ¨le gÃ©omÃ©trique direct pour la configuration 2
 plot(LRMate200i,q2)
 axis('equal')
 title('Configuration q2 = [45 60 30 120 170 45]');
@@ -66,7 +63,7 @@ disp('appuyer sur une ENTREE')
 pause();
 clf
 
-%Calcul du modèle géométrique direct pour la configuration 3
+%Calcul du modÃ¨le gÃ©omÃ©trique direct pour la configuration 3
 plot(LRMate200i,q3)
 axis('equal')
 title('Configuration q3 = [90 30 90 90 90 90]')
@@ -79,59 +76,59 @@ clf
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%-------------------Calcul du modèle géométrique inverse-------------------
+%-------------------Calcul du modÃ¨le gÃ©omÃ©trique inverse-------------------
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%ikine : calcule les positions articulaires en radian à partir de la
-%matrice homogène correspondant au passage du repère R0 au repère de bout
+%ikine : calcule les positions articulaires en radian Ã  partir de la
+%matrice homogÃ¨ne correspondant au passage du repÃ¨re R0 au repÃ¨re de bout
 %de chaine.
 
-%transl : construit la matrice homogène correspondant à la translation
-%définie par un vecteur.
+%transl : construit la matrice homogÃ¨ne correspondant Ã  la translation
+%dÃ©finie par un vecteur.
 
-%rotx,roty,rotz : construisent les matrices homogènes correspondant à une
+%rotx,roty,rotz : construisent les matrices homogÃ¨nes correspondant Ã  une
 %rotation respectivement autour de x,y,z
 
-%MatHomn : Matrice homogène globale composée des matrices homogènes de
+%MatHomn : Matrice homogÃ¨ne globale composÃ©e des matrices homogÃ¨nes de
 %translations et de rotation
 
 
-%Définition de la postition et orientation de l'organe terminale
+%DÃ©finition de la postition et orientation de l'organe terminale
 X1=[500 0 600 45 45 30];
 X2=[450 450 500 0 0 90];
 X3=[100 500 400 90 45 0];
 
-%Construction des matrices homogènes
+%Construction des matrices homogÃ¨nes
 MatHom1=transl(X1(1:3))*rotz(X1(6)*pi/180)*roty(X1(5)*pi/180)*rotx(X1(4)*pi/180);
 MatHom2=transl(X2(1:3))*rotz(X2(6)*pi/180)*roty(X2(5)*pi/180)*rotx(X2(4)*pi/180);
 MatHom3=transl(X3(1:3))*rotz(X3(6)*pi/180)*roty(X3(5)*pi/180)*rotx(X3(4)*pi/180);
 
-%Modèle géométrique inverse
+%ModÃ¨le gÃ©omÃ©trique inverse
 qq1=mod(ikine(LRMate200i,MatHom1,q1),2*pi)*180/pi
 qq2=mod(ikine(LRMate200i,MatHom2,q1),2*pi)*180/pi
 qq3=mod(ikine(LRMate200i,MatHom3,q1),2*pi)*180/pi
 
-%Vérification du modèle pour les paramètre q1
+%VÃ©rification du modÃ¨le pour les paramÃ¨tre q1
 MatHomV1=[0 -0 1 540;0 -1 -0 -0;1 0 -0 675;0 0 0 1 ];
 qqV1=mod(ikine(LRMate200i,MatHomV1,q1),2*pi)*180/pi;
 
-% Tracé des positions du Robot
+% TracÃ© des positions du Robot
 plot(LRMate200i,qq1*pi/180)
 axis('equal')
-title('Configuration X1 = [500 0 600 45° 45° 30°]');
+title('Configuration X1 = [500 0 600 45Â° 45Â° 30Â°]');
 disp('appuyer sur une ENTREE')
 pause()
 clf
 plot(LRMate200i,qq2*pi/180)
 axis('equal')
-title('Configuration X2 = [450 450 500 0° 0° 90°]');
+title('Configuration X2 = [450 450 500 0Â° 0Â° 90Â°]');
 disp('appuyer sur une ENTREE')
 pause()
 clf
 plot(LRMate200i,qq3*pi/180)
 axis('equal')
-title('Configuration X3 = [100 500 400 90° 45° 0°]');
+title('Configuration X3 = [100 500 400 90Â° 45Â° 0Â°]');
 disp('appuyer sur une ENTREE')
 pause()
 clf
